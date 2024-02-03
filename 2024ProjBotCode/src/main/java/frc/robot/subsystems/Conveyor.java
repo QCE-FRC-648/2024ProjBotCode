d20094a7b0773805b2e23d949a76d142d2c5fd8b
QC-Elite-648 <>
2024-02-03 13:45:31 -0600

    made conveyor run using operator left joystick

--- 2024ProjBotCode/src/main/java/frc/robot/subsystems/Conveyor.java
+++ 2024ProjBotCode/src/main/java/frc/robot/subsystems/Conveyor.java
@@ -1,24 +1,42 @@
 package frc.robot.subsystems;
 import frc.robot.Constants.ConveyorConstants;
-import frc.robot.Constants.ConveyorConstants;
 import com.revrobotics.CANSparkMax;
 import edu.wpi.first.wpilibj.XboxController;
 
 public class Conveyor {
 
     public CANSparkMax conveyorMotor;
-
+    public XboxController operator;
+    public double leftY;
 
     public Conveyor() {
         conveyorMotor = new CANSparkMax(ConveyorConstants.conveyorCAN, ConveyorConstants.motorType);
         conveyorMotor.set(0);
+
+        operator = new XboxController(1);
     }
 
-/* 
-    @Override 
-    public void periodic() 
-    {
-        
+    //the left joystick controlls the conveyor 
+    public void runConveyorMotor() {
+
+        //gets the value of the operator's left joystick
+        leftY = operator.getLeftY();
+
+        //go up conveyor
+        if(leftY > 0.3) {
+            conveyorMotor.set(leftY);
+        }
+
+        //go down conveyor
+        else if(leftY < -0.3) {
+            conveyorMotor.set(leftY);
+        }
+
+        //stop conveyor
+        else {
+            conveyorMotor.set(0);
+        }
     }
-*/
+
+    
 }
