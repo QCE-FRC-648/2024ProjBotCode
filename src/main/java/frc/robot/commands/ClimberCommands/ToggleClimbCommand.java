dc0592d64a881f3d6bd313c006a8881d82fc6665
Jackson W <90067155+jweatherspoon7@users.noreply.github.com>
2024-02-15 19:09:47 -0600

    Added new directories for commands

---
+++ src/main/java/frc/robot/commands/ClimberCommands/ToggleClimbCommand.java
@@ -0,0 +1,8 @@
+package frc.robot.commands.ClimberCommands;
+
+import edu.wpi.first.wpilibj2.command.Command;
+
+public class ToggleClimbCommand extends Command
+{
+    
+}
--- src/main/java/frc/robot/commands/IntakeCommands/IntakeConveyorCommand.java
+++ src/main/java/frc/robot/commands/IntakeCommands/IntakeConveyorCommand.java
@@ -1,4 +1,4 @@
-package frc.robot.commands;
+package frc.robot.commands.IntakeCommands;
 
 import com.ctre.phoenix.motorcontrol.ControlMode;
 
