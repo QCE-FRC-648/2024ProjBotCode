80c5409f465cddf5bda36d6a78ad420d3b56ef6c
QC-Elite-648 <vishwaspatel2006@gmail.com>
2024-02-17 16:07:52 -0600

    made climb

--- src/main/java/frc/robot/commands/ClimberCommands/ToggleClimbCommand.java
+++ src/main/java/frc/robot/commands/ClimberCommands/ToggleClimbCommand.java
@@ -1,39 +1,49 @@
 package frc.robot.commands.ClimberCommands;
 
-import com.ctre.phoenix.motorcontrol.ControlMode;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.ClimberSubsystem;
-import edu.wpi.first.wpilibj.Timer;
 
 public class ToggleClimbCommand extends Command
 {
     private final ClimberSubsystem climber;
+    private int num;
 
     public ToggleClimbCommand(ClimberSubsystem subsystem)
     {
         climber = subsystem;
+        num = 0;
     }
 
     @Override
-    public void initialize() { }
+    public void initialize() {
+        num++;
+    }
 
     @Override
     public void execute() {
-        climber.climber1.set(ControlMode.PercentOutput, 0.1);
-        climber.climber2.set(ControlMode.PercentOutput, 0.1);
+        if(num%2 == 1) {
+            climber.climber1.set(0.1);
+            climber.climber2.set(0.1);
+        }
+        else {
+            climber.climber1.set(-0.1);
+            climber.climber2.set(-0.1);
+        }
     }
 
     @Override
     public boolean isFinished()
     {
-        Timer.delay(1);
-        return true;
+        if(climber.climber1.getMotorOutputVoltage() > 40 || climber.climber2.getMotorOutputVoltage() > 40){ 
+            return true;
+        }
+        return false;
     }
 
     @Override 
     public void end(boolean interrupted)
     {
-        climber.climber1.set(ControlMode.PercentOutput, 0);
-        climber.climber2.set(ControlMode.PercentOutput, 0);
+        climber.climber1.set(0);
+        climber.climber2.set(0);
     }
 }
