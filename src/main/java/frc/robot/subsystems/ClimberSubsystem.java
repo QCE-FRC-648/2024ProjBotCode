5e4be7b4bbaeaadfa69775259f1a0b37f4de96d8
Jackson W <90067155+jweatherspoon7@users.noreply.github.com>
2024-02-29 15:42:13 -0600

    Fixed PDH object

--- src/main/java/frc/robot/subsystems/ClimberSubsystem.java
+++ src/main/java/frc/robot/subsystems/ClimberSubsystem.java
@@ -3,6 +3,7 @@ package frc.robot.subsystems;
 import com.ctre.phoenix.motorcontrol.NeutralMode;
 import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
 import edu.wpi.first.wpilibj.PowerDistribution;
+import edu.wpi.first.wpilibj.PowerDistribution.ModuleType;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.ClimberConstants;
@@ -17,7 +18,7 @@ public class ClimberSubsystem extends SubsystemBase
     {
         climber1.setNeutralMode(NeutralMode.Brake);
         climber2.setNeutralMode(NeutralMode.Brake);
-        PDH = new PowerDistribution();
+        PDH = new PowerDistribution(1, ModuleType.kRev);
     }
 
     public double getCurrent(int num) 
